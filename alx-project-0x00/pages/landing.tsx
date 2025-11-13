f692dbee02176b43dab1c318e9a46f53c97e3b1b
Nthabiseng <molekwanthabiseng@gmail.com>
2025-11-13 11:08:14 +0200

    All added files

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import Card from "@/components/Card"
+import Button from "@/components/Button"
+
+const Landing: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
+
+      {/* Cards */}
+      <Card />
+      <Card />
+
+      {/* Buttons */}
+      <div className="flex flex-wrap gap-4 mt-6">
+        <Button title="Small Button" size="small" shape="rounded-sm" />
+        <Button title="Medium Button" size="medium" shape="rounded-md" />
+        <Button title="Large Button" size="large" shape="rounded-full" />
+      </div>
+    </div>
+  )
+}
+
+export default Landing
