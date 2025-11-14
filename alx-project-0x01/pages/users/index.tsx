11bae9b7a99197747ee28607add3a00293afcf37
Nthabiseng <molekwanthabiseng@gmail.com>
2025-11-14 07:25:38 +0200

    Added files and intitial files into the directory

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,63 @@
+import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import UserModal from "@/components/common/UserModal";
+import { UserData, UserProps } from "@/interfaces";
+import { useState } from "react";
+
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [users, setUsers] = useState(posts);
+
+  // Function to handle adding a new user
+  const handleAddUser = (newUser: UserData) => {
+    const updatedUser = { ...newUser, id: users.length + 1 };
+    setUsers([...users, updatedUser]);
+  };
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-2xl font-semibold">Users</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white hover:bg-blue-800 transition"
+          >
+            Add User
+          </button>
+        </div>
+
+        {/* User Cards */}
+        <div className="grid grid-cols-3 gap-4">
+          {users?.map((user) => (
+            <UserCard key={user.id} {...user} />
+          ))}
+        </div>
+      </main>
+
+      {/* Modal for Adding User */}
+      {isModalOpen && (
+        <UserModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddUser}
+        />
+      )}
+    </div>
+  );
+};
+
+// Fetch user data at build time
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Users;
+
